4d1a7966143e04337aa696a8b29028e843dfd4c6
mensur056 <mansur.sarxanov@gmail.com>
2022-04-20 10:01:09 +0400

    'test'

--- day_2/lib/101/image_learn.dart
+++ day_2/lib/101/image_learn.dart
@@ -10,12 +10,14 @@ class ImageLearn extends StatelessWidget {
       body: Column(
         children: [
           SizedBox(
-            height: 300,
+            height: 100,
             width: 400,
             child: Image.asset(
               ImageItems().imageName,
             ),
-          )
+          ),
+          Image.network(
+              'https://toppng.com/uploads/preview/spider-man-png-clipart-spiderman-11563069948whuzsyplwv.png')
         ],
       ),
     );

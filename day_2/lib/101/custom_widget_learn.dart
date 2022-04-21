fa9244f262452414328fc7791d58fd12e33e8d84
mensur056 <mansur.sarxanov@gmail.com>
2022-04-21 09:51:18 +0400

    'test'

--- day_2/lib/101/custom_widget_learn.dart
+++ day_2/lib/101/custom_widget_learn.dart
@@ -9,21 +9,48 @@ class CustomWidgetLearn extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            shape: const StadiumBorder(),
-            primary: ColorsUtility().redColor,
-          ),
-          onPressed: () {},
-          child: Padding(
-            padding: PaddingUtility().padding,
-            child: Text(
-              title,
-              style: Theme.of(context).textTheme.subtitle1?.copyWith(
-                  color: ColorsUtility().whiteColor,
-                  fontWeight: FontWeight.bold),
+        child: Column(
+          children: [
+            CustomButton(title: title),
+            const SizedBox(
+              height: 26,
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SizedBox(
+                child: CustomButton(title: title),
+                width: MediaQuery.of(context).size.width,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: const StadiumBorder(),
+        primary: ColorsUtility().redColor,
+      ),
+      onPressed: () {},
+      child: Padding(
+        padding: PaddingUtility().padding,
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.subtitle1?.copyWith(
+              color: ColorsUtility().whiteColor, fontWeight: FontWeight.bold),
         ),
       ),
     );

47f721984b798436e6256a2ced748bdf64063ecc
mensur056 <mansur.sarxanov@gmail.com>
2022-04-22 13:34:32 +0400

    'test'

--- day_2/lib/101/page_view.dart
+++ day_2/lib/101/page_view.dart
@@ -1,5 +1,6 @@
 import 'package:day_2/101-KnowledgeApp-2/knowledge_app_2.dart';
 import 'package:day_2/101-KnowledgeApp/knowledge_app.dart';
+import 'package:day_2/101/button_learn.dart';
 import 'package:day_2/101/icon_learn.dart';
 import 'package:day_2/101/indicator_learn.dart';
 import 'package:flutter/material.dart';
@@ -12,13 +13,37 @@ class PageViewLearn extends StatefulWidget {
 }
 
 class _PageViewLearnState extends State<PageViewLearn> {
+  final _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: PageView(
-        children: [IconLearnView(),const KnowledgeApp(), const KnowledgeApp2()],
+        controller: _pageController,
+        children: const [ButtonLearn(), KnowledgeApp(), KnowledgeApp2()],
+      ),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              _pageController.previousPage(duration: _duration.DurationLow, curve: Curves.easeInOutCubicEmphasized);
+            },
+            child: const Icon(Icons.chevron_left),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              _pageController.nextPage(duration: _duration.DurationLow, curve: Curves.easeOutSine);
+            },
+            child: const Icon(Icons.chevron_right),
+          ),
+        ],
       ),
     );
   }
+}
+
+class _duration {
+  static const DurationLow = Duration(seconds: 1);
 }

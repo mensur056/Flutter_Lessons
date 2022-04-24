b2b7adb46aaacbffd5b16b0a170baf8a1dcd7ecf
mensur056 <mansur.sarxanov@gmail.com>
2022-04-24 19:06:33 +0400

    'test'

--- day_2/lib/101/list_view_learn.dart
+++ day_2/lib/101/list_view_learn.dart
@@ -23,6 +23,27 @@ class _ListViewLearnState extends State<ListViewLearn> {
             height: 300,
             color: Theme.of(context).colorScheme.error,
           ),
+          Container(
+            height: 300,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Container(
+                  width: 300,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 300,
+                  color: Colors.green,
+                ),
+                const Placeholder(),
+                Container(
+                  width: 300,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );

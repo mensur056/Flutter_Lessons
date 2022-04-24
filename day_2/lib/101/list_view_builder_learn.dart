0293405b225bc62133152db883ab940718386e0b
mensur056 <mansur.sarxanov@gmail.com>
2022-04-24 19:22:52 +0400

    'test'

--- day_2/lib/101/list_view_builder_learn.dart
+++ day_2/lib/101/list_view_builder_learn.dart
@@ -12,15 +12,23 @@ class _ListViewBuilderLearnState extends State<ListViewBuilderLearn> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView.builder(itemBuilder: (context, index) {
-        print(index);
-        return SizedBox(
-          height: 200,
-          child: Column(
-            children: [Expanded(child: Image.network('https://picsum.photos/200/300')), Text('$index')],
-          ),
-        );
-      }),
+      body: ListView.separated(
+        separatorBuilder: (context, index) {
+          return const Divider(
+            color: Colors.white,
+          );
+        },
+        itemBuilder: (context, index) {
+          print(index);
+          return SizedBox(
+            height: 200,
+            child: Column(
+              children: [Expanded(child: Image.network('https://picsum.photos/200/300')), Text('$index')],
+            ),
+          );
+        },
+        itemCount: 10,
+      ),
     );
   }
 }

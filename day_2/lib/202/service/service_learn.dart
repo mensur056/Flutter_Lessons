4c4055f229c57f556f886c5a486ade49690700a9
mensur056 <mansur.sarxanov@gmail.com>
2022-04-27 11:10:00 +0400

    'test'

--- day_2/lib/202/service/service_learn.dart
+++ day_2/lib/202/service/service_learn.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:day_2/202/service/post_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +13,7 @@ class ServiceLearn extends StatefulWidget {
 
 class _ServiceLearnState extends State<ServiceLearn> {
   List<PostModel>? _items;
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -18,15 +21,35 @@ class _ServiceLearnState extends State<ServiceLearn> {
     fetchPostModel();
   }
 
+  void changeLoading() {
+    setState(() {
+      _isLoading = !_isLoading;
+    });
+  }
+
   Future<void> fetchPostModel() async {
+    changeLoading();
     final response = await Dio().get('https://jsonplaceholder.typicode.com/posts');
+    if (response.statusCode == HttpStatus.ok) {
+      final _datas = response.data;
+      if (_datas is List) {
+        setState(() {
+          _items = _datas.map((e) => PostModel.fromJson(e)).toList();
+        });
+      }
+      changeLoading();
+    }
+
     print(response);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Mansur'),
+        actions: [_isLoading ? const CircularProgressIndicator() : const SizedBox.shrink()],
+      ),
       body: ListView.builder(
         itemBuilder: (context, index) {
           return const Text('data');

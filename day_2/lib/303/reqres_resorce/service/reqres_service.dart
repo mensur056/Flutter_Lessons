fb45b098f5875cc38007dfbd1c46552f47314fc4
mensur056 <81297953+mensur056@users.noreply.github.com>
2022-06-19 11:06:28 +0400

    test

---
+++ day_2/lib/303/reqres_resorce/service/reqres_service.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:day_2/303/reqres_resorce/model/resource_model.dart';
+import 'package:dio/dio.dart';
+
+abstract class IReqresService {
+  IReqresService(this.dio);
+  final Dio dio;
+
+  Future<List<ResourceModel>?> fetchResourceItems();
+}
+
+class ResourceService extends IReqresService {
+  ResourceService(Dio dio) : super(dio);
+
+  @override
+  Future<List<ResourceModel>?> fetchResourceItems() async {
+    final response = await dio.get('https://reqres.in/api/unknown');
+    if (response.statusCode == HttpStatus.ok) {
+      final jsonBody = response.data;
+      if (jsonBody is List) {
+        return jsonBody.map((json) => ResourceModel.fromJson(json)).toList();
+      }
+    }
+    return null;
+  }
+}
